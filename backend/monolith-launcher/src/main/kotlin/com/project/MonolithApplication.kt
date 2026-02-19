3872e8c901608ec541e3846d4dc84114dcd304f0
jiwon.kim <jiwon.kim01@e4net.net>
2026-02-19 11:36:21 +0900

    deploy Dockerfile test(8) back oom

--- backend/monolith-launcher/src/main/kotlin/com/project/MonolithApplication.kt
+++ backend/monolith-launcher/src/main/kotlin/com/project/MonolithApplication.kt
@@ -1,16 +1,24 @@
 package com.project
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.data.jdbc.JdbcRepositoriesAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 @EnableFeignClients(basePackages = ["com.sideproject"]) // 👈 수정
+@EnableJpaRepositories(basePackages = ["com.sideproject.api", "com.sideproject.auth"]) // JPA 범위 한정
+@EnableRedisRepositories(basePackages = ["com.sideproject.api"]) // Redis 범위 한정
 @SpringBootApplication(
-    scanBasePackages = [
-        "com.sideproject.common",
-        "com.sideproject.auth",
-        "com.sideproject.api"
-    ]
+    // 1. 사용하지 않는 JDBC 자동 설정을 꺼서 메모리 절약
+    exclude = [
+        DataSourceTransactionManagerAutoConfiguration::class,
+        JdbcRepositoriesAutoConfiguration::class
+    ],
+    // 2. 스캔 범위 명시
+    scanBasePackages = ["com.sideproject"]
 )
 class MonolithApplication
 
