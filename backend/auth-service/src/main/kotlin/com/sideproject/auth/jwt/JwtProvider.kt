3769f24b48744e04b41847bbce50bde81963d06c
jiwon.kim <jiwon.kim01@e4net.net>
2026-02-25 11:34:43 +0900

    (back) jwt 만료 시간

--- backend/auth-service/src/main/kotlin/com/sideproject/auth/jwt/JwtProvider.kt
+++ backend/auth-service/src/main/kotlin/com/sideproject/auth/jwt/JwtProvider.kt
@@ -24,8 +24,7 @@ class JwtProvider(
             .setSubject(user.email)
             .claim("userId", user.id)
             .setIssuedAt(now)
-            .setExpiration(Date(now.time + Duration.ofSeconds(30).toMillis()))
-//            .setExpiration(Date(now.time + 1000 * 60 * 6000 ))
+            .setExpiration(Date(now.time + 1000 * 60 * 6000 ))
             .signWith(key, SignatureAlgorithm.HS256)
             .compact()
     }
